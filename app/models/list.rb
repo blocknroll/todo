ed7e2e37c6bfb601a4edef8638e839ffb612ef4f
Fred_Block <info@fredblock.com>
2013-08-24 10:30:35 -0600

    Initial commit

---
+++ app/models/list.rb
@@ -0,0 +1,7 @@
+class List < ActiveRecord::Base
+  attr_accessible :description, :name
+
+  validates :name, :presence => true
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'fdc8019a21480715e3627c371e7f1545a6f78521615bec87fd22186beaf2f5167b074bc028224def9c671c9b1f8e17e8363714d5a159f2905b8860809d79484a'
